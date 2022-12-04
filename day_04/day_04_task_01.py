bd954ea372afec887a743aeb2822b6086dd01f8b
Vova Semerikov <tinctura@gmail.com>
2022-12-04 14:32:10 +0600

    day 04 solved

--- day_04/day_04_task_01.py
+++ day_04/day_04_task_01.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 INPUT = "day_04_input.txt"
 
@@ -8,3 +8,22 @@ def read_data(file_path: str) -> List[str]:
     with open(file_path, 'r') as fin:
         data = fin.readlines()
     return data
+
+
+def get_clean_pair(raw_pair: str) -> Tuple:
+    clean_pair = [_.split("-") for _ in raw_pair.strip().split(",")]
+    return [int(_) for _ in clean_pair[0]], [int(_) for _ in clean_pair[1]]
+
+
+def count_full_pairs(all_pairs: List[str]) -> int:
+    counter = 0
+    for pair in all_pairs:
+        l, r = get_clean_pair(pair)
+        if l[0] <= r[0] and l[1] >= r[1] or l[0] >= r[0] and l[1] <= r[1]:
+            counter += 1
+    return counter
+
+
+if __name__ == "__main__":
+    data = read_data(INPUT)
+    print(count_full_pairs(data))
--- day_04/day_04_task_02.py
+++ day_04/day_04_task_02.py
@@ -1,10 +1,33 @@
-from typing import List, TextIO
+from typing import List, Tuple
 
 INPUT = "day_04_input.txt"
 
 
-def read_data(file: TextIO) -> List:
+def read_data(file_path: str) -> List[str]:
     """Read the data from file"""
-    with open(file, 'r') as fin:
+    with open(file_path, 'r') as fin:
         data = fin.readlines()
     return data
+
+
+def get_clean_pair(raw_pair: str) -> Tuple:
+    clean_pair = [_.split("-") for _ in raw_pair.strip().split(",")]
+    return [int(_) for _ in clean_pair[0]], [int(_) for _ in clean_pair[1]]
+
+
+def count_full_pairs(all_pairs: List[str]) -> int:
+    counter = 0
+    for pair in all_pairs:
+        l, r = get_clean_pair(pair)
+        if l[0] <= r[0] and l[1] >= r[1] or l[0] >= r[0] and l[1] <= r[1]:
+            counter += 1
+        elif l[0] <= r[0] and l[1] >= r[0] and l[1] <= r[1]:
+            counter += 1
+        elif l[0] >= r[0] and l[1] >= r[0] and l[1] >= r[1] and l[0] <= r[1]:
+            counter += 1
+    return counter
+
+
+if __name__ == "__main__":
+    data = read_data(INPUT)
+    print(count_full_pairs(data))
