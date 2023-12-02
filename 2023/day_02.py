d82802e53c51a184e0e1083d69919cfa55ae3af6
Vova <tinctura@gmail.com>
2023-12-02 14:38:46 +0600

    day02 part 2

--- 2023/day_02.py
+++ 2023/day_02.py
@@ -1,30 +1,43 @@
+import re
+from collections import Counter
+from math import prod
+
 import aoc_lube
-import re 
 
 LINES = aoc_lube.fetch(year=2023, day=2).splitlines()
 
-condition = {
+condition = Counter({
         "red": 12,
         "green": 13,
         "blue": 14
-    }
+    })
 
 
 def part_one():
     result = 0
-    game_state = True
     for line in LINES:
         game_n, game_log = line[5:].split(":")
-        game = re.findall(r"(\d+) (\w+)", game_log)
-        for round_ in game:
-            if condition[round_[1]] < int(round_[0]):
-                game_state = False
-                continue
-        if game_state:
+        game_cubes = Counter()
+        for n, color in re.findall(r"(\d+) (\w+)", game_log):
+            if int(n) > game_cubes[color]:
+                game_cubes[color] = int(n)
+        if game_cubes <= condition:
             result += int(game_n)
-        game_state = True
+    return result
+
+
+def part_two():
+    result = 0
+    for line in LINES:
+        game_n, game_log = line[5:].split(":")
+        game_cubes = Counter()
+        for n, color in re.findall(r"(\d+) (\w+)", game_log):
+            if int(n) > game_cubes[color]:
+                game_cubes[color] = int(n)
+        result += prod(game_cubes.values())
     return result
 
 
 if __name__ == "__main__":
-    print(part_one())
+    print(f"part one answer is {part_one()}")
+    print(f"part two answer is {part_two()}")
